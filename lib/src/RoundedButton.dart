e6c5ef43b6f7f9995331edda60f67ab6445d7e17
kururu-abdo <wdalhajana50@gmail.com>
2022-01-17 03:39:19 -0800

    first

---
+++ lib/src/RoundedButton.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  double width;
+  double height;
+  Color color;
+  double radius;
+  String text;
+  RoundedButton(
+      {Key? key,
+      this.color = Colors.green,
+      this.height = 80,
+      this.width = 250,
+      this.radius= 25,
+      this.text = 'button'})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius),
+        color: color),
+    );
+  }
+}
